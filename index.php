6ad79160a2e818212047006de0bdf113baf69c9c
30-Supavita <140419800+30-Supavita@users.noreply.github.com>
2024-07-12 21:53:53 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>30-Supavita</title>
+</head>
+<body>
+    <?php
+        $a=3;
+        $b=4;
+        $c=$a+$b;
+        $A=20.5;
+        $B=20.5;
+        $C=$A==$B;
+        echo "<h3>ผลลัพธ์</h3>";
+        echo "ค่าของ \$a = 3<br>ค่าของ \$b = 4<br>";
+        echo "ผลรวมของ \$a และ \$b คือ $c<br>";
+        echo "<h3>ผลลัพธ์</h3>";
+        echo "\$A = B<br>";
+        echo "\$A = 20.5<br>";
+        echo "\$B = 20.5<br>";
+        echo "ค่าของ \$C คือ $C (ฺเป็นค่า Boolean)";
+    ?>
+</body>
+</html>
